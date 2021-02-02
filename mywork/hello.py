40386d04fa289b5dfbd687e057b2da612d46de8b
amandamurray2021 <77901195+amandamurray2021@users.noreply.github.com>
2021-02-02 20:48:08 +0000

    Delete mywork directory

--- mywork/hello.py
+++
@@ -1,5 +0,0 @@
-# hello.py
-# This program prints out the word Hello and a name
-# Author: Amanda Murray
-
-print ("Hello Amanda") 
--- mywork/multiply.py
+++
@@ -1,4 +0,0 @@
-# multiply.py
-# This program outputs what 111 x 555 is
-# Author: Amanda Murray
-
