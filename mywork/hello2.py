8f75ff2c5e442f1f62a7dc2a1cd3edadadaad2a6
Amanda Murray <G00398364@gmit.ie>
2021-02-02 20:48:59 +0000

    Lab 2.2

--- mywork/hello2.py
+++ mywork/hello2.py
@@ -2,7 +2,7 @@
 # This program reads in a person's name and prints out hello and their name
 # Author: Amanda Murray
 
-name = input ("Amanda Murray:")
+name = input("Amanda Murray:")
 print('Hello '+ name + '\nNice to meet you')
-print('Hello {} \nNice to meet you'.format (name))
+print('Hello {} \nNice to meet you'.format(name))
 
